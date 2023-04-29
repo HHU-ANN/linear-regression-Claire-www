115c6319c59e375001d9382f0293b719ad493087
Claire-www <131932510+Claire-www@users.noreply.github.com>
2023-04-29 18:41:11 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -28,23 +28,41 @@ def ridge(data):
 def lasso(data):
     # 加载数据
     X_train, y_train = read_data()
+    # 标准化处理数据
+    X_mean = np.mean(X_train, axis=0)
+    X_std = np.std(X_train, axis=0)
+    X_train = (X_train - X_mean) / X_std
+    y_mean = np.mean(y_train)
+    y_std = np.std(y_train)
+    y_train = (y_train - y_mean) / y_std
     # 添加常数列
     X_train = np.hstack((np.ones((X_train.shape[0], 1)), X_train))
     # Lasso回归
-    def lasso_regression(X, y, alpha, max_iter=1000, tol=1e-4, eta=0.01):
+    def lasso_regression(X, y, alpha, max_iter=1000, tol=1e-4, eta=0.01, decay=0.9):
         beta = np.zeros(X.shape[1])
         for i in range(max_iter):
-            beta_new = beta - eta * X.T @ (X @ beta - y) + alpha * np.sign(beta)
+            # 计算梯度
+            grad = X.T @ (X @ beta - y) + alpha * np.sign(beta)
+            # 动态调整学习率
+            eta *= decay
+            # 更新权重
+            beta_new = beta - eta * grad
+            # L1正则化
+            beta_new = np.sign(beta_new) * np.maximum(np.abs(beta_new) - alpha * eta, 0)
             if np.linalg.norm(beta_new - beta) < tol:
                 break
             beta = beta_new
+        # 反标准化
+        beta = beta / np.hstack(([1], X_std))
+        beta[0] = y_mean - np.sum(beta[1:] * X_mean / X_std)
         return beta
     # 默认alpha=1
     alpha = 1
     beta = lasso_regression(X_train, y_train, alpha)
     # 预测
+    data = (data - X_mean) / X_std
     data = np.hstack((1, data))
-    prediction = data @ beta
+    prediction = (data @ beta[1:] + beta[0]) * y_std + y_mean
     return prediction
 
 def read_data(path='./data/exp02/'):
