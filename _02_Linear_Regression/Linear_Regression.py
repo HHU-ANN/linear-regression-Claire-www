7dc2c07f1d3377e7613dec31461e9b164fd426c4
Claire-www <131932510+Claire-www@users.noreply.github.com>
2023-05-05 18:55:38 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -65,8 +65,8 @@ def lasso(data_input):
         prediction = data @ beta
         prediction = prediction * y_std + y_mean
         return prediction
-    # 默认alpha=0.00000001
-    alpha = 0.00000001
+    # 默认alpha=0.0000000000000000000000000001
+    alpha = 0.0000000000000000000000000001
     prediction = lasso_regression(X_train, y_train, alpha, max_iter=1000, tol=1e-4, eta=0.01, decay=0.9)
     # 预测
     prediction = prediction[0]
